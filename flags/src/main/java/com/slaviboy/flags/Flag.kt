3a9615fc7e171ba5987d01f2e24f74e7e7a93423
Stanislav Krasimirov Georgiev <slavi94slavi94@gmail.com>
2023-07-09 15:25:04 +0300

    Implement string resource for country name

--- flags/src/main/java/com/slaviboy/flags/Flag.kt
+++ flags/src/main/java/com/slaviboy/flags/Flag.kt
@@ -1,44 +1,45 @@
 package com.slaviboy.flags
 
 import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
 
 sealed class Flag(
-    val country: String,
     val countryCode: String,
     val iso: String,
+    @StringRes val countryResId: Int,
     @DrawableRes val imageResId: Int
 ) {
-    object Bulgaria : Flag("Bulgaria", "+359", "bg", R.drawable.flag_bg)
-    object UnitedKingdom : Flag("UnitedKingdom", "+44", "gb", R.drawable.flag_gb)
-    object Ireland : Flag("Ireland", "+353", "ie", R.drawable.flag_ie)
-    object Island : Flag("Island", "+354", "is", R.drawable.flag_is)
-    object France : Flag("France", "+33", "fr", R.drawable.flag_fr)
-    object Lithuania : Flag("Lithuania", "+370", "lt", R.drawable.flag_lt)
-    object Latvia : Flag("Latvia", "+371", "lv", R.drawable.flag_lv)
-    object Greece : Flag("Greece", "+30", "gr", R.drawable.flag_gr)
-    object Macedonia : Flag("Macedonia", "+389", "mk", R.drawable.flag_mk)
-    object Serbia : Flag("Serbia", "+381", "rs", R.drawable.flag_rs)
-    object Romania : Flag("Romania", "+40", "ro", R.drawable.flag_ro)
-    object Turkey : Flag("Turkey", "+90", "tr", R.drawable.flag_tr)
-    object Russia : Flag("Russia", "+7", "ru", R.drawable.flag_ru)
-    object Ukraine : Flag("Ukraine", "+380", "ua", R.drawable.flag_ua)
-    object Albania : Flag("Albania", "+355", "al", R.drawable.flag_al)
-    object Spain : Flag("Spain", "+34", "es", R.drawable.flag_es)
-    object Portugal : Flag("Portugal", "+351", "pt", R.drawable.flag_pt)
-    object Poland : Flag("Poland", "+48", "pl", R.drawable.flag_pl)
-    object Italy : Flag("Italy", "+39", "it", R.drawable.flag_it)
-    object Germany : Flag("Germany", "+49", "de", R.drawable.flag_de)
-    object Sweden : Flag("Sweden", "+46", "se", R.drawable.flag_se)
-    object Norway : Flag("Norway", "+47", "no", R.drawable.flag_no)
-    object China : Flag("China", "+86", "cn", R.drawable.flag_cn)
-    object Japan : Flag("Japan", "+81", "jp", R.drawable.flag_jp)
-    object Philippines : Flag("Philippines", "+63", "ph", R.drawable.flag_ph)
-    object India : Flag("India", "+91", "in", R.drawable.flag_in)
-    object Indonesia : Flag("Indonesia", "+62", "id", R.drawable.flag_id)
-    object Pakistan : Flag("Pakistan", "+92", "pk", R.drawable.flag_pk)
-    object Mexico : Flag("Mexico", "+52", "mx", R.drawable.flag_mx)
-    object Belarus : Flag("Belarus", "+375", "by", R.drawable.flag_by)
-    object Georgia : Flag("Georgia", "+995", "ge", R.drawable.flag_ge)
+    object Bulgaria : Flag("+359", "bg", R.string.bulgaria, R.drawable.flag_bg)
+    object UnitedKingdom : Flag("+44", "gb", R.string.united_kingdom, R.drawable.flag_gb)
+    object Ireland : Flag("+353", "ie", R.string.ireland, R.drawable.flag_ie)
+    object Island : Flag("+354", "is", R.string.island, R.drawable.flag_is)
+    object France : Flag("+33", "fr", R.string.france, R.drawable.flag_fr)
+    object Lithuania : Flag("+370", "lt", R.string.lithuania, R.drawable.flag_lt)
+    object Latvia : Flag("+371", "lv", R.string.latvia, R.drawable.flag_lv)
+    object Greece : Flag("+30", "gr", R.string.greece, R.drawable.flag_gr)
+    object Macedonia : Flag("+389", "mk", R.string.macedonia, R.drawable.flag_mk)
+    object Serbia : Flag("+381", "rs", R.string.serbia, R.drawable.flag_rs)
+    object Romania : Flag("+40", "ro", R.string.romania, R.drawable.flag_ro)
+    object Turkey : Flag("+90", "tr", R.string.turkey, R.drawable.flag_tr)
+    object Russia : Flag("+7", "ru", R.string.russia, R.drawable.flag_ru)
+    object Ukraine : Flag("+380", "ua", R.string.ukraine, R.drawable.flag_ua)
+    object Albania : Flag("+355", "al", R.string.albania, R.drawable.flag_al)
+    object Spain : Flag("+34", "es", R.string.spain, R.drawable.flag_es)
+    object Portugal : Flag("+351", "pt", R.string.portugal, R.drawable.flag_pt)
+    object Poland : Flag("+48", "pl", R.string.poland, R.drawable.flag_pl)
+    object Italy : Flag("+39", "it", R.string.italy, R.drawable.flag_it)
+    object Germany : Flag("+49", "de", R.string.germany, R.drawable.flag_de)
+    object Sweden : Flag("+46", "se", R.string.sweden, R.drawable.flag_se)
+    object Norway : Flag("+47", "no", R.string.norway, R.drawable.flag_no)
+    object China : Flag("+86", "cn", R.string.china, R.drawable.flag_cn)
+    object Japan : Flag("+81", "jp", R.string.japan, R.drawable.flag_jp)
+    object Philippines : Flag("+63", "ph", R.string.philippines, R.drawable.flag_ph)
+    object India : Flag("+91", "in", R.string.india, R.drawable.flag_in)
+    object Indonesia : Flag("+62", "id", R.string.indonesia, R.drawable.flag_id)
+    object Pakistan : Flag("+92", "pk", R.string.pakistan, R.drawable.flag_pk)
+    object Mexico : Flag("+52", "mx", R.string.mexico, R.drawable.flag_mx)
+    object Belarus : Flag("+375", "by", R.string.belarus, R.drawable.flag_by)
+    object Georgia : Flag("+995", "ge", R.string.georgia, R.drawable.flag_ge)
 
     companion object {
         fun allFlags(): List<Flag> {
