cd2ab069febb18e118ebaa65d413e3b6241500da
slaviboy <slavi94slavi94@gmail.com>
2023-09-30 00:10:50 +0300

    Add slider

--- app/src/main/java/com/slaviboy/flagsexample/MainActivity.kt
+++ app/src/main/java/com/slaviboy/flagsexample/MainActivity.kt
@@ -4,19 +4,28 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Slider
+import androidx.compose.material3.Text
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.slaviboy.flags.Country
 
@@ -24,42 +33,45 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            var sliderPosition by remember { mutableStateOf(1f) }
             Column(
-                Modifier.verticalScroll(rememberScrollState())
+                modifier = Modifier
+                    .verticalScroll(rememberScrollState())
+                    .padding(top = 100.dp)
+                    .padding(horizontal = 30.dp)
             ) {
-                Country.values().forEach {
-                    Row {
-                        Image(
-                            modifier = Modifier
-                                .height(100.dp)
-                                .wrapContentWidth()
-                                .padding(10.dp),
-                            contentScale = ContentScale.FillHeight,
-                            painter = painterResource(id = it.icon1x1ResId),
-                            contentDescription = null
-                        )
-                        Image(
-                            modifier = Modifier
-                                .height(100.dp)
-                                .wrapContentWidth()
-                                .padding(10.dp)
-                                .clip(CircleShape),
-                            contentScale = ContentScale.FillHeight,
-                            painter = painterResource(id = it.icon1x1ResId),
-                            contentDescription = null
-                        )
-                        Image(
-                            modifier = Modifier
-                                .height(100.dp)
-                                .wrapContentWidth()
-                                .padding(10.dp)
-                                .clip(RoundedCornerShape(10.dp)),
-                            contentScale = ContentScale.FillHeight,
-                            painter = painterResource(id = it.icon4x3ResId),
-                            contentDescription = null
-                        )
-                    }
+                Country.values().forEachIndexed { index, country ->
+                    if (index < 20)
+                        Row(
+                            verticalAlignment = Alignment.CenterVertically
+                        ) {
+                            Image(
+                                modifier = Modifier
+                                    .height(50.dp)
+                                    .wrapContentWidth()
+                                    .padding(10.dp)
+                                    .clip(RoundedCornerShape(15.dp * sliderPosition)),
+                                contentScale = ContentScale.FillHeight,
+                                painter = painterResource(id = country.icon1x1ResId),
+                                contentDescription = null
+                            )
+                            Text(text = stringResource(id = country.countryResId))
+                        }
                 }
+            }
+            Column(
+                modifier = Modifier
+                    .padding(horizontal = 30.dp)
+                    .background(Color.White)
+            ) {
+                Text(text = "Round corners:")
+                Slider(
+                    value = sliderPosition,
+                    onValueChange = {
+                        sliderPosition = it
+                    },
+                    steps = 10
+                )
             }
         }
     }
