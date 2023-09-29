fabc5bc1cd83e8541616beb39558d39d6a46d291
slaviboy <slavi94slavi94@gmail.com>
2023-09-29 23:33:27 +0300

    Add 3 types preview - rect, circle, rounded corners

--- app/src/main/java/com/slaviboy/flagsexample/MainActivity.kt
+++ app/src/main/java/com/slaviboy/flagsexample/MainActivity.kt
@@ -5,12 +5,16 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
@@ -24,15 +28,37 @@ class MainActivity : ComponentActivity() {
                 Modifier.verticalScroll(rememberScrollState())
             ) {
                 Country.values().forEach {
-                    Image(
-                        modifier = Modifier
-                            .height(100.dp)
-                            .wrapContentWidth()
-                            .padding(10.dp),
-                        contentScale = ContentScale.FillHeight,
-                        painter = painterResource(id = it.icon1x1ResId),
-                        contentDescription = null
-                    )
+                    Row {
+                        Image(
+                            modifier = Modifier
+                                .height(100.dp)
+                                .wrapContentWidth()
+                                .padding(10.dp),
+                            contentScale = ContentScale.FillHeight,
+                            painter = painterResource(id = it.icon1x1ResId),
+                            contentDescription = null
+                        )
+                        Image(
+                            modifier = Modifier
+                                .height(100.dp)
+                                .wrapContentWidth()
+                                .padding(10.dp)
+                                .clip(CircleShape),
+                            contentScale = ContentScale.FillHeight,
+                            painter = painterResource(id = it.icon1x1ResId),
+                            contentDescription = null
+                        )
+                        Image(
+                            modifier = Modifier
+                                .height(100.dp)
+                                .wrapContentWidth()
+                                .padding(10.dp)
+                                .clip(RoundedCornerShape(10.dp)),
+                            contentScale = ContentScale.FillHeight,
+                            painter = painterResource(id = it.icon4x3ResId),
+                            contentDescription = null
+                        )
+                    }
                 }
             }
         }
